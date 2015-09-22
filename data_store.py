91b381aa3984cd46bde8be9bf005ece3080e4daf
Raj Krishnan <rajkrishnan1996@gmail.com>
2015-09-23 00:19:08 +0530

    Initial implementation of data storage in csv

---
+++ data_store.py
@@ -0,0 +1,24 @@
+import os.path
+
+csv = {}
+
+def generate_csv(name):
+	if name not in csv:
+		csv_new = None
+		if os.path.isfile(name):
+			csv_new = open('name', 'a')
+		else:
+			csv_new = open('name', 'w')
+			csv_new.write('Week, Quantity, Amount, Address\n')
+		csv[name] = csv_new
+		return 0
+	return -1
+
+def close_csv():
+	for key in csv.keys():
+		csv[key].close()
+
+def add_data(week, item, quantity, amount, address):
+	if item not in csv.keys():
+		generate_csv(item)
+	csv[item].write(str(week) + ', ' + str(quantity) + ', ' + str(amount) + ', ' + str(address) + '\n')
