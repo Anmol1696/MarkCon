30eb5a4c1aebaabea1983a886045ee7a266c21db
Raj Krishnan <rajkrishnan1996@gmail.com>
2015-09-23 00:43:47 +0530

    Changed data_store.py

--- data_store.py
+++ data_store.py
@@ -1,6 +1,17 @@
 import os.path
 
 csv = {}
+item_list = []
+items = {}
+address_mapping = {}
+
+
+class Item:
+	def __init__(name):
+		self.name = name
+		self.quantity = 0
+		self.amount = 0
+		self.address = [0 for i in range(len(address_mapping))]
 
 def generate_csv(name):
 	if name not in csv.keys:
@@ -10,7 +21,7 @@ def generate_csv(name):
 			csv_new.write('\n')
 		else:
 			csv_new = open((name + '.csv'), 'w')
-			csv_new.write('Week, Quantity, Amount, Address\n')
+			csv_new.write('Week, Quantity, Amount, ADDRESSLIST\n')
 		csv[name] = csv_new
 		return 0
 	return -1
@@ -19,7 +30,18 @@ def close_csv():
 	for key in csv.keys():
 		csv[key].close()
 
-def add_data(week, item, quantity, amount, address):
-	if item not in csv.keys():
+def add_data(item, quantity, amount, address):
+	if item in item_list:
+		if item not in items.keys():
+			items[item] = Item(item)
+		items[item].quantity += quantity
+		items[item].amount += amount
+		items[item].address[address_mapping[address]] += 1
+
+def load_data(week):
+	if item not in csv.keys() and item in item_list:
 		generate_csv(item)
-	csv[item].write(str(week) + ', ' + str(quantity) + ', ' + str(amount) + ', ' + str(address) + '\n')
+	for name, item in items:
+		# Need to implement address storage after info is received
+		csv[name].write(str(week) + ', ' + str(item.quantity) + ', ' + str(item.amount) + ', ' + str(item.address) + '\n')
+	items = {}
