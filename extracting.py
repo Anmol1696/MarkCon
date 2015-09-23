3c6088bf3e9a40822ac95d05a8e12c66cbe59e3c
Anmol1696 <anmol1696@gmail.com>
2015-09-23 10:08:11 +0530

    Update

--- extracting.py
+++ extracting.py
@@ -1,11 +1,29 @@
-from data_store import data_log
+from data_store import *
+ref_week = [63]
 
-def extract(address, items_s, date):
+def extract(address, item_s, date):
+	print date
 	address = ' '.join(str(address).split(" ")[2:-4])
 
 	for item_a in item_s:
 		item_mod = (str(item_a).split(" ")[:-4])
 		cost = int(str(item_a).split(" ")[-1])
 		quantity = int(str(item_a).split(" ")[-3])
+		week = date_week(date)
 
-		data_log(item_mod, quantity, cost, address, date)
+		if week < ref_week[-1]:
+			load_data(week)
+			ref_week.append(week)
+		else: pass
+
+		add_data(item_mod, quantity, cost, address)
+
+def date_week(date):
+	date = str(date).split("-")
+	print date
+	if int(date[0]) == 2015 or (int(date[1]) == 12 and int(date[2] > 28)):
+			add = 24
+	else: add = -28
+	print int(date[0])
+	week = datetime.date(int(date[0]), int(date[1]), int(date[2])).isocalendar()[1] + add
+	return week
