3ad8471752e4a6c16c162c930e5f4f6d6f2f0222
Anmol1696 <anmol1696@gmail.com>
2015-10-03 19:42:17 +0530

    Update after seperating login.py

--- page_scrape.py
+++ page_scrape.py
@@ -1,27 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from login import web_driver
 import time
-
-def web_driver(email = 'anmol.y@zabbed.com', password = 'marcon123'):
-	baseurl = 'https://www.facebook.com/login'
-	driver = webdriver.Firefox()
-	driver.get(baseurl)
-
-	xpaths = {
-		'email' : "//input[@name = 'email']",
-		'password' : "//input[@name = 'pass']",
-		'login' : "//input[@name = 'login']",
-	}
-
-	try:
-		driver.find_element_by_xpath(xpaths['email']).send_keys(email)
-		driver.find_element_by_xpath(xpaths['password']).send_keys(password)
-		driver.find_element_by_xpath(xpaths['login']).click()
-	except:
-		return 0
-
-	return driver
-
 
 def open_page(page_url = 'https://www.facebook.com/juicifix'):
 	driver = web_driver()
