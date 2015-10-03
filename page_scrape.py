1c5bfeab78a299f51c1ee30a95f86ce8eaab14a6
Anmol1696 <anmol1696@gmail.com>
2015-10-03 19:37:59 +0530

    Getting all the profile of the users

--- page_scrape.py
+++ page_scrape.py
@@ -32,24 +32,39 @@ def open_page(page_url = 'https://www.facebook.com/juicifix'):
 def pagelikes():
 	driver = open_page()
 	time.sleep(5)
-	scrol_number = 10
 	like_links = []
-	# while scrol_number > 0:
-	# 	driver.find_element_by_xpath('//div').send_keys(Keys.PAGE_DOWN)
-	# 	list_likes = driver.find_elements_by_xpath("//div[@class = 'UFILikeSentenceText']")
-	# 	for like in list_likes:
-	# 		like_links.append(like.get_attribute('href'))
-	# 	print like_links
-	# 	scrol_number -= 1
+	profile_links = []
 
-	driver.find_element_by_xpath('//div').send_keys(Keys.PAGE_DOWN)
-	time.sleep(1)
-	list_likes = driver.find_elements_by_xpath("//div[@class = 'UFILikeSentenceText']")
-	for l in list_likes:
-		print l.get_attribute("href")
-		print l.text
+	for _ in range(2):
+		driver.find_element_by_xpath('//div').send_keys(Keys.PAGE_DOWN)
+		time.sleep(1)
 
+	for element_like in driver.find_elements_by_xpath("//div[@class = 'UFILikeSentenceText']//a[@class='UFINoWrap']"):
+		if element_like.get_attribute('href') not in like_links:
+			like_links.append(element_like.get_attribute('href'))
+
+	for element_like in driver.find_elements_by_xpath("//div[@class = 'UFILikeSentenceText']//a[@class='profileLink']"):
+		if element_like.get_attribute('href') not in profile_links:
+			profile_links.append(element_like.get_attribute('href'))
+
+	profile_from_links = get_profile(driver, like_links[2:])
+	for profile in profile_from_links:
+		if profile not in profile_links:
+			profile_links.append(profile)
+
+	return profile_links[1:]
+
+def get_profile(driver, like_links):
+	profile_links = []
+	for url in like_links:
+		driver.get(url)
+		time.sleep(3)
+		temp = driver.find_elements_by_xpath("//li[@class = 'fbProfileBrowserListItem']//div[@class = 'fsl fwb fcb']/a")
+		for element in temp:
+			if element.get_attribute('href') not in profile_links:
+				profile_links.append(element.get_attribute('href'))
+	return profile_links
 
 
 if __name__ == '__main__':
-	pagelikes()
+	print pagelikes()
