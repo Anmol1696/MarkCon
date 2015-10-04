f4bb0dc7aaa246436846b1d322fcc5c0b9c8e6bc
Anmol1696 <anmol1696@gmail.com>
2015-10-04 16:28:58 +0530

    Adding the csv file for similarpages

--- form_csv.py
+++ form_csv.py
@@ -2,8 +2,15 @@ def csv_form():
 	csv_file = open('people_info.csv', 'w')
 	csv_file.write("Name, Gender, Lives, Job, Education\n")
 	csv_file.close()
+	csv_file = open('similar_pages.csv', 'w')
+	csv_file.close()
 
 def add_info(person):
 	csv_file = open('people_info.csv', 'a')
 	csv_file.write("%s, %s, %s, %s, %s\n" % (person.name, person.sex, person.lives, person.job, person.education))
+	csv_file.close()
+
+def similar_pages_csv(page):
+	csv_file = open('similar_pages.csv', 'a')
+	csv_file.write(page + '\n')
 	csv_file.close()
