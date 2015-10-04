5b1ea2a44702accdeb0f4d9f0588f43ada5d9691
Anmol1696 <anmol1696@gmail.com>
2015-10-04 13:41:56 +0530

    Minor corrections

--- profile.py
+++ profile.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from login import web_driver
 from form_csv import *
 import time
+import unicodedata
 
 xpaths = {
 	'name' : "//div[@class = '_6-e']",
@@ -31,11 +32,11 @@ def main_profile(profile_links, driver):
 def single_profile(url, driver):
 	driver.get(url + u'/about')
 	person = Info(url)
-	person.name = getting_name(driver)
-	person.sex = getting_sex(driver)
-	person.lives = getting_lives(driver)
-	person.job = getting_job_education(driver, 'job')
-	person.education = getting_job_education(driver, 'education')
+	person.name = unicodedata.normalize('NFKD', getting_name(driver)).encode('ascii','ignore').replace(',','-')
+	person.sex = unicodedata.normalize('NFKD', getting_sex(driver)).encode('ascii','ignore').replace(',','-')
+	person.lives = unicodedata.normalize('NFKD', getting_lives(driver)).encode('ascii','ignore').replace(',','-')
+	person.job = unicodedata.normalize('NFKD', getting_job_education(driver, 'job')).encode('ascii','ignore').replace(',','-')
+	person.education = unicodedata.normalize('NFKD', getting_job_education(driver, 'education')).encode('ascii','ignore').replace(',','-')
 
 	add_info(person)
 
@@ -45,29 +46,30 @@ def getting_name(driver):
 def getting_sex(driver):
 	try:
 		text = str(driver.find_element_by_xpath(xpaths['sex']).text)
+		print text
 	except: return '-'
 
-	if "him" in text or "he" in text:
-		return 'M'
-	elif "she" in text or "her" in text:
-		return 'F'
-	else: return '-'
+	if " him " in text or " he " in text:
+		return u'M'
+	elif " she " in text or " her " in text:
+		return u'F'
+	else: return u"-"
 
 def getting_lives(driver):
 	try:
 		location_text = driver.find_element_by_xpath(xpaths['lives']).text.split('\n')[0]
 	except:
-		return '-'
+		return u'-'
 
 	if 'No' not in location_text:
 		return location_text.split("Lives in")[1]
-	else: return '-'
+	else: return u'-'
 
 def getting_job_education(driver, var):
 	job_text = driver.find_element_by_xpath(xpaths[var]).text.split('\n')[0]
 	if 'No' not in job_text:
 		return job_text
-	else: return '-'
+	else: return u'-'
 
 if __name__ == "__main__":
 	single_profile()
