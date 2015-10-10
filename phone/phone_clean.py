616710793f71ad73f180b08aea6e6622c0c28fd9
Anmol1696 <anmol1696@gmail.com>
2015-10-10 21:06:24 +0530

    script to extract phone numbers

---
+++ phone/phone_clean.py
@@ -0,0 +1,29 @@
+def read_lines(name_file = "Numbers.csv"):
+	f = open(name_file, 'r')
+	lines = f.readlines()
+	f.close()
+	return lines
+
+def main():
+	lines = read_lines()
+	numbers = []
+
+	f = open('new_number.csv', 'a')
+	for line in lines:
+		number = line[:-1].split(",")[1]
+		if type(number) is str:
+			try:
+				number = int(number)
+			except:
+				pass
+		if type(number) is int and int(str(number)[0]) > 6 and str(number) not in numbers:
+			if len(str(number)) >= 10:
+				if len(str(number)) == 12:
+					number = int(str(number)[2:])	
+				numbers.append(str(number))
+				f.write(str(number) + ',\n')
+
+	f.close()
+
+if __name__ == '__main__':
+	main()
