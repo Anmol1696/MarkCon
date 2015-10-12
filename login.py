d38adfaa1e90c7bd186c3d0abfc43c316c51f559
Anmol1696 <anmol1696@gmail.com>
2015-10-13 02:18:08 +0530

    Removing personal info

--- login.py
+++ login.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-def web_driver(email = 'anmol.y@zabbed.com', password = 'marcon123'):
+def web_driver(email = '', password = ''):
 	baseurl = 'https://www.facebook.com/login'
 	profile = webdriver.FirefoxProfile()
 	profile.set_preference("network.proxy.type", 0)
 	driver = webdriver.Firefox(profile)
+	driver.set_page_load_timeout(30)
 	driver.get(baseurl)
 
 	xpaths = {
