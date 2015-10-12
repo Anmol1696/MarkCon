1601a045bf2cebedef004d990154fc4399012350
Anmol1696 <anmol1696@gmail.com>
2015-10-13 02:18:27 +0530

    Cleanup

--- page_scrape.py
+++ page_scrape.py
@@ -4,54 +4,46 @@ from login import web_driver
 from profile import main_profile
 import time
 
-
-def open_page(page_url = 'https://www.facebook.com/juicifix'):
-	driver = web_driver()
+def open_page(number, driver):
+	# driver = web_driver()
+	page_url = "https://www.facebook.com/search/str/%s/keywords_users" % (str(number))
 	driver.get(page_url)
 	time.sleep(2)
 	return driver
 
-def pagelikes():
-	driver = open_page()
-	time.sleep(3)
+def get_profile_id(driver):
+	xpaths = {
+		'profile_id' : "//div[@class = 'clearfix']//a[@class = '_8o _8s lfloat _ohe']"
+	}
 
-	num_scroll = 200
-	like_links = []
-	profile_links = []
-
-	for _ in range(num_scroll):
-		driver.find_element_by_xpath('//div').send_keys(Keys.PAGE_DOWN)
-		time.sleep(1)
-
-	for element_like in driver.find_elements_by_xpath("//div[@class = 'UFILikeSentenceText']//a[@class='UFINoWrap']"):
-		if element_like.get_attribute('href') not in like_links:
-			like_links.append(element_like.get_attribute('href'))
-
-	for element_like in driver.find_elements_by_xpath("//div[@class = 'UFILikeSentenceText']//a[@class='profileLink']"):
-		if element_like.get_attribute('href').split('?')[0] not in profile_links:
-			profile_links.append(element_like.get_attribute('href').split('?')[0])
-
-	profile_from_links = get_profile(driver, like_links[2:])
-	for profile in profile_from_links:
-		if profile not in profile_links:
-			profile_links.append(profile)
-
-	return (profile_links[1:], driver)
-
-def get_profile(driver, like_links):
-	profile_links = []
-	for url in like_links:
-		driver.get(url)
-		time.sleep(3)
-		temp = driver.find_elements_by_xpath("//li[@class = 'fbProfileBrowserListItem']//div[@class = 'fsl fwb fcb']/a")
-		for element in temp:
-			if element.get_attribute('href').split('?')[0] not in profile_links:
-				profile_links.append(element.get_attribute('href').split('?')[0])
-	return profile_links
+	profile = driver.find_element_by_xpath(xpaths['profile_id']).get_attribute('href')
+	return (profile, driver)
 
 def main():
-	temp = pagelikes()
-	main_profile(temp[0], temp[1])
+	profiles = []
+	f = open("phone/new_number.csv", 'r')
+	numbers = f.readlines()
+	f.close()
+
+	f = open("profiles_1.csv", 'a')
+
+	print len(numbers)
+
+	driver = web_driver()
+
+	for number in numbers:
+		number = int(number[:-2])
+		try:
+			temp = get_profile_id(open_page(number, driver))
+			profile = temp[0].split("?")[0]
+			driver = temp[1]
+			if profile not in profiles:
+				profiles.append(profile)
+				f.write(profile + ',\n')
+				print profile
+		except:
+			pass
+	main_profile(profiles, driver)
 
 if __name__ == '__main__':
 	main()
